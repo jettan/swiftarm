f35e2074ad94752696d389c91cfc5aec1d79ca6a
Jethro Tan <jethrotan20@gmail.com>
2012-03-22 16:43:17 +0100

    Make compiled binary of swift run on the D7000.

--- libevent2/samsung-build.sh
+++ libevent2/samsung-build.sh
@@ -1,21 +1,20 @@
 #!/bin/sh -x
 #
 
-SRC_DIR="/home/jettan/Documents/swiftarm/libevent2"
-INSTALL_DIR="/home/jettan/Documents/swiftarm/lib"
+INSTALL_DIR="/home/jettan/swiftarm/lib"
 
 
-cd $SRC_DIR
+cd $(HOME)/swiftarm/libevent2
 
 export CC="arm-v7a8-linux-gnueabi-gcc"
-export CCFLAGS="-static"
+export CFLAGS="-static"
 export LD="arm-v7a8-linux-gnueabi-ld"
 export AR="arm-v7a8-linux-gnueabi-ar"
 export RANLIB="arm-v7a8-linux-gnueabi-ranlib"
 export STRIP="arm-v7a8-linux-gnueabi-strip"
-#export LDFLAGS="-rpath /home/jettan/Documents/swiftarm/libevent2/include/event2"$LDFLAGS
-export LDADD="-lgcc -lc -lrt"
+export LDADD="/opt/armv7a8-toolchain-lite/arm-v7a8-linux-gnueabi/libc/usr/lib/libc.a /opt/armv7a8-toolchain-lite/arm-v7a8-linux-gnueabi/libc/usr/lib/librt.a /opt/armv7a8-toolchain-lite/lib/gcc/arm-v7a8-linux-gnueabi/4.4.1/libgcc.a"
+export LIBS="$LDADD"
 
-./configure --host=arm-v7a8-linux-gnueabi --prefix=$INSTALL_DIR LIBS="-lc -lgcc -lrt"
+./configure --host=arm-v7a8-linux-gnueabi --prefix=$INSTALL_DIR --disable-shared
 
 exit 0
