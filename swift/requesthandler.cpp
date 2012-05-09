4b2fbbdc302238f915c0ff645c480ed8bf8ddf37
Jethro Tan <jethrotan20@gmail.com>
2012-05-09 13:02:30 +0200

    Clean up requesthandler and attempt threading.

--- swift/requesthandler.cpp
+++ swift/requesthandler.cpp
@@ -29,17 +29,9 @@
 #include "socketlistener.h"
 #include "request.h"
 
-#define checkResults(string, val) {                                                        \
-	if (val) {                                                                             \
-		printf("Failed with %d at %s.", val, string);                                      \
-		exit(1);                                                                           \
-	}                                                                                      \
-}                                                                                          \
-
 typedef struct str {
 	char* tracker;
 	char* hash;
-	int filesize;
 	char* filename;
 } DownloadArgs;
 
@@ -50,7 +42,6 @@ int rc;
 DownloadArgs download_args;
 struct event evcompl;
 int download;
-int size;
 
 CRequestHandler::CRequestHandler() {
 	rc = 0;
@@ -64,7 +55,7 @@ CRequestHandler::CRequestHandler() {
 }
 
 void IsCompleteCallback(int fd, short event, void *arg) {
-	if (swift::SeqComplete(download) != size)
+	if (swift::SeqComplete(download) != swift::Size(download))
 		evtimer_add(&evcompl, swift::tint2tv(TINT_SEC));
 	else
 		event_base_loopexit(swift::Channel::evbase, NULL);
@@ -75,7 +66,6 @@ void IsCompleteCallback(int fd, short event, void *arg) {
  * Download a file using libswift.
  * @param tracker: The tracker (x.x.x.x:port).
  * @param hash: The root merkle hash (40 characters).
- * @param filesize: The filesize of the file.
  * @param filename: The name to be given to the downloaded file.
  */
 void* Download(void *str) {
@@ -83,12 +73,26 @@ void* Download(void *str) {
 	download         = 0;
 	char* tracker    = da->tracker;
 	char* hash       = da->hash;
-	size             = da->filesize;
 	char* filename   = da->filename;
 	
 	// Make a socket to listen to.
 	evutil_socket_t sock = INVALID_SOCKET;
-	sock = swift::Listen(swift::Address("127.0.0.1:32997"));
+	swift::Address bindaddress;
+	for (int i = 0; i < 10; i++) {
+		bindaddress = swift::Address((uint32_t) INADDR_ANY, 0);
+		sock = swift::Listen(swift::Address(bindaddress));
+		
+		if (sock > 0) {
+			break;
+		}
+		
+		if (sock == 9) {
+			std::cerr << "Could not listen to any socket." << std::endl;
+			pthread_exit(NULL);
+		}
+	}
+	
+	std::cout << "Listening on port " << swift::BoundAddress(sock).port() << "." << std::endl;
 	
 	swift::Address trackeraddr = swift::Address(tracker);
 	swift::Sha1Hash root_hash = swift::Sha1Hash(true, hash);
@@ -111,16 +115,25 @@ void* Download(void *str) {
 		// Dispatch the event base to enter the swift loop.
 		event_base_dispatch(swift::Channel::evbase);
 		
-		std::cout << "Download completed." << std::endl;
+		std::cout << "Download of " << filename << " completed." << std::endl;
 		// Close the file.
 		swift::Close(download);
+		
+		std::cout << "Shutting down listener." << std::endl;
+		// Shutdown the listener port.
+		swift::Shutdown(sock);
 	}
 	
 	std::cout << "Exiting download thread." << std::endl;
-	// Close the download thread when the download is finished.
+	// Exit the download thread when the download is finished.
 	pthread_exit(NULL);
 }
 
+
+/**
+ * The HTTP request command handler.
+ * @param *pRequest: The request received.
+ */
 bool CRequestHandler::Handle(CRequest *pRequest) {
 	if (strncmp(pRequest->GetCommand(), "GET", 3) == 0) {
 		std::cout << "Received GET command." << std::endl;
@@ -129,6 +142,10 @@ bool CRequestHandler::Handle(CRequest *pRequest) {
 	return true;
 }
 
+/**
+ * The handler for all GET commands.
+ * Commands can be "download" and "stop_download".
+ */
 void CRequestHandler::HandleGET(CRequest *pRequest) {
 	int val = 1;
 	if (setsockopt(pRequest->GetHandle().iSock, SOL_TCP, TCP_CORK, & val, 4) == -1) {
@@ -136,101 +153,45 @@ void CRequestHandler::HandleGET(CRequest *pRequest) {
 		return;
 	}
 	
-	char pBuf[8192];
-	memset(pBuf, 0, sizeof(pBuf));
+	char message[8192];
+	memset(message, 0, sizeof(message));
 	
-	char pFileName[128];
-	memset(pFileName, 0, sizeof(pFileName));
-	if (strcmp(pRequest->GetPath(), "/test.txt") == 0) {
-		sprintf(pFileName, "test.txt");
+	if (strcmp(pRequest->GetPath(), "/stop") == 0) {
+		std::cout << "Stopping download..." << std::endl;
 		
-	} else if (strcmp(pRequest->GetPath(), "/close_download") == 0) {
-		std::cout << "Attempt to join download thread with main thread..." << std::endl;
+		pthread_join(thread,(void**) &rc);
 		
-		// Join the threads.
-		void* status;
-		rc = pthread_join(thread, &status);
-		checkResults("joining download thread with main thread", rc);
+		if (rc) {
+			std::cerr << "ERROR: Join failed! Code: " << rc << "." << std::endl;
+		}
 		
-		sprintf(pBuf, "HTTP/1.1 200 OK\n"
+		sprintf(message, "HTTP/1.1 200 OK\n"
 			"Content-Type: text/plain\n"
-			"Content-Length: 16\n\n"
-			"Closed download.");
-		pRequest->Write(pBuf, strlen(pBuf));
-		return;
-		
+			"Content-Length: 17\n\n"
+			"Closed download.\n");
+		pRequest->Write(message, strlen(message));
 	} else if (strcmp(pRequest->GetPath(), "/download") == 0) {
 		
 		// Fill in the neccessary arguments to download a file.
 		download_args.tracker  = "127.0.0.1:20000";
 		download_args.hash     = "ed29d19bc8ea69dfb5910e7e20247ee7e002f321";
-		download_args.filesize = 61382756;
 		download_args.filename = "stream.mp4";
 		
 		// Spawn new thread to download the file requested.
-		int retval = pthread_create(&thread, NULL, &Download, (void*) &download_args);
+		rc = pthread_create(&thread, NULL, &Download, (void*) &download_args);
 		
-		sprintf(pBuf, "HTTP/1.1 200 OK\n"
+		if (rc) {
+			std::cerr << "ERROR: failed to create download thread. Code: " << rc << "." << std::endl;
+		}
+		
+		sprintf(message, "HTTP/1.1 200 OK\n"
 			"Content-Type: text/plain\n"
-			"Content-Length: 17\n\n"
-			"Download started.");
-		pRequest->Write(pBuf, strlen(pBuf));
-		return;
+			"Content-Length: 18\n\n"
+			"Download started.\n");
+		pRequest->Write(message, strlen(message));
 		
 	} else {
-		std::cout << "Nothing to do here..." << std::endl;
+		std::cerr << "Bad HTTP GET request." << std::endl;
 	}
-	
-	int iFile = open(pFileName, O_RDONLY);
-	if (iFile < 0) {
-		if (errno == EACCES) {
-			sprintf(pBuf, "HTTP/1.1 403 Forbidden\n\n");
-		} else if (errno == ENOENT) {
-			sprintf(pBuf, "HTTP/1.1 404 File not found\n\n");
-		} else {
-			std::cerr << "Error: open failed, unknown errno: " << errno << std::endl;
-			sprintf(pBuf, "HTTP/1.1 404 File not found\n\n");
-		}
-		pRequest->Write(pBuf, strlen(pBuf));
-		return;
-	}
-	
-	struct stat buf;
-	memset(&buf, 0, sizeof(buf));
-	
-	if (fstat(iFile, &buf) != 0) {
-		std::cerr << "error, fstat failed for: " << pFileName << std::endl;
-		close(iFile);
-		return;
-	}
-	
-	// Check file type
-	char *pFileType = pFileName + (strlen(pFileName) - 5);
-	if (strcmp(pFileType + 1, ".txt") == 0) {
-		std::cout << "Sending the header..." << std::endl;
-		sprintf(pBuf, "HTTP/1.1 200 OK\n"
-			"Accept-Ranges: none\n"
-			"Keep-Alive: timeout=15, max=100\n"
-			"Connection: Keep-Alive\n"
-			"Content-Type: text/plain\n"
-			"Content-Length: %ld\n\n", buf.st_size);
-	}
-	
-	pRequest->Write(pBuf, strlen(pBuf));
-	
-	std::cout << "Sending the message..." << std::endl;
-	ssize_t bytestowrite = buf.st_size;
-	off_t offset = 0;
-	ssize_t byteswritten = pRequest->WriteFile(iFile, bytestowrite);
-	std::cout << "Sent the message." << std::endl;
-	
-	if (byteswritten != bytestowrite) {
-		std::cerr << "error, bytestowrite = " << bytestowrite << ", byteswritten = " << byteswritten << std::endl;
-		close(iFile);
-		return;
-	}
-	
-	close(iFile);
-	
 	return;
 }
