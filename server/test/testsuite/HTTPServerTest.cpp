9180845f4d1911bc2f142251593d34330c458e7d
Samuel Austin <samuelaustin_3@hotmail.com>
2012-06-08 17:35:46 +0200

    Resolved conflicts.

--- server/test/testsuite/HTTPServerTest.cpp
+++ server/test/testsuite/HTTPServerTest.cpp
@@ -1,15 +1,75 @@
+#include <string>
+
+#include "curl.h"
+#include "easy.h"
 #include "HttpServer.h"
 #include "gtest.h"
-#include <string>
 
 class HTTPServerTest : public ::testing::Test {
 	protected:
 	
+	CURL *easyHandle;
+	CURLcode res;
+	std::string response;
+	
 	virtual ~HTTPServerTest() {}
+	
+	size_t static getResponse(char *response, size_t size, size_t count, void *stream) {
+		
+		((std::string*)stream)->append((char*)response, 0, size * count);
+		return size*count;
+	}
 	
 	virtual void SetUp(){
 		
+		easyHandle = curl_easy_init();
+		curl_easy_setopt(easyHandle, CURLOPT_WRITEFUNCTION, &getResponse);
+		curl_easy_setopt(easyHandle, CURLOPT_WRITEDATA, &response);
+		
 	}
 	
-	virtual void TearDown() {}
+	virtual void TearDown() {
+		
+		curl_easy_cleanup(easyHandle);
+	}
 };
+
+TEST_F(HTTPServerTest, aliveTest) {
+	
+	curl_easy_setopt(easyHandle, CURLOPT_URL, "http://127.0.0.1:1337/alive");
+	res = curl_easy_perform(easyHandle);
+	
+	EXPECT_EQ("Alive", response);
+}
+
+TEST_F(HTTPServerTest, searchTrivial) {
+	
+	EXPECT_EQ(0, SearchEngine::getResults().size());
+	
+	curl_easy_setopt(easyHandle, CURLOPT_URL, "http://127.0.0.1:1337/search:test");
+	res = curl_easy_perform(easyHandle);
+	
+	EXPECT_LT(0, SearchEngine::getResults().size());
+}
+
+TEST_F(HTTPServerTest, searchEmpty) {
+	
+	EXPECT_EQ(0, SearchEngine::getResults().size());
+	
+	curl_easy_setopt(easyHandle, CURLOPT_URL, "http://127.0.0.1:1337/search:");
+	res = curl_easy_perform(easyHandle);
+	
+	EXPECT_EQ(0, SearchEngine::getResults().size());
+	EXPECT_EQ("Invalid Search Term", response);
+}
+
+TEST_F(HTTPServerTest, downloadTrivial) {
+	
+	curl_easy_setopt(easyHandle, CURLOPT_URL, "http://127.0.0.1:1337/search:test");
+	res = curl_easy_perform(easyHandle);
+	
+	curl_easy_setopt(easyHandle, CURLOPT_URL, "http://127.0.0.1:1337/download:367d26a6ce626e049a21921100e24eac86dbcd32";);
+	res = curl_easy_perform(easyHandle);
+	
+	EXPECT_EQ(response, "Download Started");
+}
--- server/test/testsuite/Main.cpp
+++ server/test/testsuite/Main.cpp
@@ -1,9 +1,76 @@
-#include "gtest.h"
 #include <iostream>
 
+#include "curl.h"
+#include "easy.h"
+#include "gtest.h"
+#include "DownloadManager.h"
+#include "swift.h"
+#include "HttpServer.h"
+#include <pthread.h>
+
+pthread_t server_thread;
+
+/**
+ * Define the InstallHTTPGateway method in httpgw.cpp.
+ */
+bool InstallHTTPGateway(struct event_base *evbase, swift::Address addr, uint32_t chunk_size, double *maxspeed);
+
+void *serverCallback(void *args) {
+	std::cout << "Entered HTTP server thread" << std::endl;
+	HttpServer::init();
+}
 
 int main(int argc, char **argv){
 	
+	// Copied from Main.cpp in ./ws
+	
+	// Enable pthread use in libevent.
+	evthread_use_pthreads();
+	
+	swift::Channel::evbase = event_base_new();
+	
+	evutil_socket_t sock = INVALID_SOCKET;
+	swift::Address bindaddress;
+	for (int i = 0; i < 10; i++) {
+		bindaddress = swift::Address((uint32_t) INADDR_ANY, 0);
+		sock = swift::Listen(swift::Address(bindaddress));
+		
+		if (sock > 0) {
+			break;
+		}
+		
+		if (sock == 9) {
+			std::cerr << "Could not listen to any socket for swift." << std::endl;
+			return -1;
+		}
+	}
+	std::cout << "Listening on port " << swift::BoundAddress(sock).port() << "." << std::endl;
+	
+	// HTTP gateway address for swift to stream.
+	//swift::Address httpaddr = swift::Address("130.161.158.52:15000");
+	swift::Address httpaddr = swift::Address("127.0.0.1:15000");
+	
+	double maxspeed[2] = {DBL_MAX, DBL_MAX};
+	
+	// Install the HTTP gateway to stream.
+	bool res = InstallHTTPGateway(swift::Channel::evbase, httpaddr, SWIFT_DEFAULT_CHUNK_SIZE, maxspeed);
+	std::cout << "HTTP gateway installation returned: " << res << std::endl;
+	
+	std::cout << "Initialised swift" << std::endl;
+	
+	// Set Download directory
+	DownloadManager::setDownloadDirectory("/dtv/usb/sda1/Downloads");
+	DownloadManager::init();
+	
+	//swift::Channel::debug_file = stdout;
+	// Make httpserver loop
+	int rc = pthread_create(&server_thread, NULL, serverCallback, NULL);
+	std::cout << "pthread_create returned: " << rc << std::endl;
+	sleep(1);
+	// curl init
+	curl_global_init(CURL_GLOBAL_ALL);
+	
+	// gtest init
 	::testing::InitGoogleTest(&argc, argv);
 	
 	return RUN_ALL_TESTS();
